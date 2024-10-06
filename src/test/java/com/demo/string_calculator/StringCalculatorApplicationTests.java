b349484623ee317980d09c82cba5bce6b1bacc5f
Aarushi Gupta <aarushigupta777@gmail.com>
2024-10-06 21:36:28 +0530

    initiating spring project

---
+++ src/test/java/com/demo/string_calculator/StringCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.string_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
