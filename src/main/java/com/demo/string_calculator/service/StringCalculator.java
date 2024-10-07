09b0015c919b8fe825bdf6b9558a1c7684985c25
Aarushi Gupta <aarushigupta777@gmail.com>
2024-10-07 09:41:03 +0530

    integrating code for 2 digit

--- src/main/java/com/demo/string_calculator/service/StringCalculator.java
+++ src/main/java/com/demo/string_calculator/service/StringCalculator.java
@@ -2,11 +2,19 @@ package com.demo.string_calculator.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 public class StringCalculator {
 
     public int add(String numbers){
-        return numbers.isEmpty() ? 0 : Integer.parseInt(numbers);
+        if(numbers.isEmpty()){
+            return 0;
+        }
+        String[] num = numbers.split(",");
+        int numSum = 0;
+        numSum = Arrays.stream(num).mapToInt(Integer::parseInt).sum();
+        return numSum;
     }
 
 }
